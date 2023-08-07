5dfd67db52874c8312f90e557ee04fac14ca954f
Savio Luan <savioluan16@gmail.com>
2023-08-07 09:17:12 -0300

    3.0

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
-import { api } from "../../Services/api";
+import { api } from "../../services/api";
 
 
 export const HomePage = ({ addCart, cartList, setIsVisible}) => { 
